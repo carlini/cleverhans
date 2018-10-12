1eb4b4db11cf7e26b8c38c81315ef32c17c8bf74
Nicholas Carlini <nicholas@carlini.com>
2018-10-12 16:35:50 +0000

    Try new pass at code architecture

--- cleverhans/report.py
+++ cleverhans/report.py
@@ -44,6 +44,13 @@ class GraphReport(Report):
     plt.plot(self.xs, self.ys)
     plt.show()
 
+class Data(object):
+  pass
+
+class SummaryData(Data):
+  pass
+
+    
 def get_accuracy(sess, X, logits, xs, ys, batch_size):
   acc = 0
   for i in range(0, len(xs), batch_size):
@@ -79,53 +86,6 @@ def report_for_attack(sess, model, x_test, y_test, X, logits, batch_size,
                       "TODO",
                       acc)
 
-def report_sweep_epsilon(sess, model, x_test, y_test, X, logits, batch_size,
-                         attack, eps_max, granularity, attack_kwargs,
-                         eps_iter_fn):
-  """
-  Generate a report for a specific attack at one set of hyperparameters but
-  varrying the epsilon distortion bound.
-
-  Instead of performing a naive sweep of all epsilon values, uses binary
-  search on a per-example basis to determine the minimal distortion.
-  """
-
-  
-  final_adv = []
-  for i in range(0, len(x_test), batch_size):
-    x_batch = x_test[i:i+batch_size]
-    y_batch = y_test[i:i+batch_size]
-    last_worked = np.copy(x_batch)
-    epsilon = np.ones((len(x_batch),1,1,1))*(eps_max/2)
-
-    os = np.zeros(500, dtype=np.bool)
-    adjust_epsilon = eps_max/4
-    while adjust_epsilon*4 >= granularity:
-      attack_kwargs['eps'] = epsilon
-      # For a fixed (epsilon, nb_iter), compute the eps_iter
-      attack_kwargs['eps_iter'] = eps_iter_fn(epsilon, attack_kwargs['nb_iter'])
-
-      adv = attack.generate_np(x_batch, y=y_batch, **attack_kwargs)
-
-      preds = np.argmax(sess.run(logits, {X: adv}), axis=1)
-
-      attack_success = preds != np.argmax(y_batch, axis=1)
-      
-      epsilon[np.logical_not(attack_success)] += adjust_epsilon
-      epsilon[attack_success] -= adjust_epsilon
-      os |= attack_success
-      last_worked[attack_success] = adv[attack_success]
-      adjust_epsilon /= 2
-    print(np.mean(os))
-    print(np.mean(epsilon))
-    final_adv.extend(last_worked)
-
-  delta = np.max(np.abs(final_adv-x_test),axis=(1,2,3))
-
-  return GraphReport("Adversarial Epsilon Sweep",
-                     "TODO",
-                     delta)
-
 def batched_report(fn):
   def wrap(_sentinel=None, **kwargs):
     if _sentinel is not None:
@@ -138,35 +98,58 @@ def batched_report(fn):
     del kwargs['x_test']
     del kwargs['y_test']
     
-    outer_result = None
+    outer_result = []
+    
     for i in range(0, len(x_test), batch_size):
       kwargs['x_test'] = x_test[i:i+batch_size]
       kwargs['y_test'] = y_test[i:i+batch_size]
 
-      batch_results = fn(**kwargs)
-
-      if outer_result is None:
-        # First batch.
-        # Compute the return type and make the data structure we're going to fill it in
-        if isinstance(batch_results, tuple()):
-          outer_result = [[] for _ in range(len(batch_results))]
-        else:
-          outer_result = [[]]
-
-      if isinstance(batch_results, tuple()):
-        for outer,batch_result in zip(outer_result,batch_results):
-          outer.append(batch_result)
-      else:
-        outer_result[0].append(batch_results)
-    ys_05 = [np.percentile(result, 5, axis=0) for result in outer_result]
-    ys_95 = [np.percentile(result, 95, axis=0) for result in outer_result]
-    ys = [np.mean(result, axis=0) for result in outer_result]
-
-
-    print(ys, ys_05, ys_95)
-    return ys, ys_05, ys_95
+      batch_result = fn(**kwargs)
+      print(batch_result)
+      outer_result.extend(batch_result)
+    return outer_result
       
   return wrap
+
+@batched_report
+def report_sweep_epsilon(sess, defended_model, x_test, y_test,
+                         X, defended_logits,
+                         attack, eps_max, granularity, attack_kwargs,
+                         eps_iter_fn):
+  """
+  Generate a report for a specific attack at one set of hyperparameters but
+  varrying the epsilon distortion bound.
+
+  Instead of performing a naive sweep of all epsilon values, uses binary
+  search on a per-example basis to determine the minimal distortion.
+  """
+
+  
+  last_worked = np.copy(x_test)
+  epsilon = np.ones((len(x_test),1,1,1))*(eps_max/2)
+
+  os = np.zeros(len(x_test), dtype=np.bool)
+  adjust_epsilon = eps_max/4
+  while adjust_epsilon*4 >= granularity:
+    attack_kwargs['eps'] = epsilon
+    # For a fixed (epsilon, nb_iter), compute the eps_iter
+    attack_kwargs['eps_iter'] = eps_iter_fn(epsilon, attack_kwargs['nb_iter'])
+
+    adv = attack.generate_np(x_test, y=y_test, **attack_kwargs)
+
+    preds = np.argmax(sess.run(defended_logits, {X: adv}), axis=1)
+
+    attack_success = preds != np.argmax(y_test, axis=1)
+    
+    epsilon[np.logical_not(attack_success)] += adjust_epsilon
+    epsilon[attack_success] -= adjust_epsilon
+    os |= attack_success
+    last_worked[attack_success] = adv[attack_success]
+    adjust_epsilon /= 2
+
+  delta = np.max(np.abs(last_worked-x_test),axis=(1,2,3))
+
+  return delta
 
 @batched_report
 def report_sweep_iterations(sess, defended_model, 
@@ -191,44 +174,38 @@ def report_sweep_iterations(sess, defended_model,
 
     attack_success = preds != np.argmax(y_test, axis=1)
 
-    success_rate.append(np.mean(attack_success))
-  print(success_rate)
+    success_rate.append(attack_success)
+    
+  return np.array(success_rate).T
 
-  return success_rate
-
-
+@batched_report
 def transfer_from_undefended(sess, defended_model, undefended_model,
                              x_test, y_test, X,
-                             defended_logits, undefended_logits, batch_size,
+                             defended_logits, undefended_logits,
                              eps_max, granularity, attack):
   """
   Generate a report that attemps a transferability test for a specific attack.
   """
-  raise 
   attack_success = []
   transfer_success = []
-  for i in range(0, len(x_test), batch_size):
-    x_batch = x_test[i:i+batch_size]
-    y_batch = y_test[i:i+batch_size]
-    for epsilon in np.arange(0,eps_max,granularity):
+  for epsilon in np.arange(0,eps_max,granularity):
 
-      adv = attack.generate_np(x_batch, y=y_batch,
-                               nb_iter=30,
-                               eps=epsilon,
-                               eps_iter=epsilon)
-      
-      preds = np.argmax(sess.run(undefended_logits, {X: adv}), axis=1)
-      batch_attack_success = preds != np.argmax(y_batch, axis=1)
+    adv = attack.generate_np(x_batch, y=y_batch,
+                             nb_iter=30,
+                             eps=epsilon,
+                             eps_iter=epsilon)
+    
+    preds = np.argmax(sess.run(undefended_logits, {X: adv}), axis=1)
+    batch_attack_success = preds != np.argmax(y_batch, axis=1)
 
-      preds_defended = np.argmax(sess.run(defended_logits, {X: adv}), axis=1)
-      batch_transfer_success = preds_defended != np.argmax(y_batch, axis=1)
+    preds_defended = np.argmax(sess.run(defended_logits, {X: adv}), axis=1)
+    batch_transfer_success = preds_defended != np.argmax(y_batch, axis=1)
 
-      attack_success += np.sum(batch_attack_success)
-      transfer_success += np.sum(batch_transfer_success)
-  return GraphReport("Transfer success epsilon sweep",
-                     "TODO",
-                     attack_success/len(x_test),
-                     transfer_success/len(x_test))
+    attack_success.append(batch_attack_success)
+    transfer_success.append(batch_transfer_success)
+
+  return np.array([attack_success, transfer_success]).T
+
 
 class Message:
   def __init__(self, severity, message):
@@ -260,6 +237,9 @@ def perform_sanity_checks(results, datset_name):
                                   "White-box iteration-sweep evaluation does not converge:"
                                   "the attack succes is still increasing."))
         
+    elif result.threatmodel == 'black_box' and isinstance(result,GraphReport):
+      if result.x_label == 'epsilon':
+        pass
 
   if dataset_name in ["mnist", "cifar-10", "imagenet"]:
     if dataset_name == "mnist":
@@ -319,7 +299,8 @@ def generate_report(sess, defended_model,
   #print(report_accuracy(sess, undefended_model, x_test, y_test, X, undefended_logits, batch_size).render())
 
   eps_iter_fn = lambda eps, steps: eps/(steps**.5)
-  
+
+  """
   print(report_sweep_iterations(sess=sess,
                                 defended_model=defended_model, 
                                 x_test=x_test,
@@ -333,6 +314,22 @@ def generate_report(sess, defended_model,
                                 attack=cleverhans.attacks.MadryEtAl(defended_model, sess=sess),
                                 attack_kwargs={'eps': .1},
                                 eps_iter_fn=eps_iter_fn))
+  """
+
+  print(report_sweep_epsilon(sess=sess,
+                                defended_model=defended_model, 
+                                x_test=x_test,
+                                y_test=y_test,
+                                X=X,
+                                defended_logits=defended_logits,
+                                batch_size=batch_size,
+                                eps_max=0.3,
+                                granularity=.01,
+                                attack=cleverhans.attacks.MadryEtAl(defended_model, sess=sess),
+                                attack_kwargs={'nb_iter': 20},
+                                eps_iter_fn=eps_iter_fn))
+
+  
   #print(transfer_from_undefended(sess, defended_model, undefended_model,
   #                               x_test, y_test, X,
   #                               defended_logits, undefended_logits, batch_size, .9, 0.1).render())
